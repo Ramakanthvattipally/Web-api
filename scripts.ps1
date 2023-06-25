c3b34f40bf59c19d525b3ba7537f5246d1688a8e
Ramakanth <ramakanthvattipally@gmail.com>
2023-06-25 13:39:35 +0100

    commiting changes for automating the WEb-API

--- scripts.ps1
+++ scripts.ps1
@@ -18,3 +18,18 @@ Compress-Archive -Path "D:\Tutorials\dotnet\user-api\UserApi\bin\Release\net7.0\
     "managementEndpointUrl": "https://management.core.windows.net/"
 }
 
+az ad sp create-for-rbac --name "apisecret" --role contributor --scopes /subscriptions/3fc031f6-75e6-4ac2-a2f4-671def6fb32e
+--sdk-auth
+
+{
+    "clientId": "888727cf-37d5-4015-8ee6-1eb882d9405e",
+    "clientSecret": "YnM8Q~kOljHJtGWq.9zRUT~DDCM3dqwy1ClD-cH6",
+    "subscriptionId": "3fc031f6-75e6-4ac2-a2f4-671def6fb32e",
+    "tenantId": "015dcbaf-7501-4b28-a8b4-732a9b4b4172",
+    "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+    "resourceManagerEndpointUrl": "https://management.azure.com/",
+    "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+    "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+    "galleryEndpointUrl": "https://gallery.azure.com/",
+    "managementEndpointUrl": "https://management.core.windows.net/"
+  }
